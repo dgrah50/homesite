668eca5cddcd81dfb361208a78214c6f94a55f03
Dayan Graham <dayangraham@dayans-mbp.home>
2024-07-16 19:41:56 +0100

    (refactor): Reuse styles.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,18 +6,29 @@ export default {
     extend: {
       colors: {
         primary: "#0D6D01",
-        background: "#1c1c1c",
-        border: "rgba(255, 255, 255, .06)",
-        headingcolor: "rgba(255, 255, 255, .95)",
-        subheadingcolor: "rgba(255, 255, 255, .75)",
-        textcolor: "rgba(255, 255, 255, .5)",
-        descriptioncolor: "rgba(255, 234, 204, .3)",
+        background: {
+          DEFAULT: "#1c1c1c",
+          dark: "#141414",
+        },
+        border: {
+          DEFAULT: "rgba(255, 255, 255, 0.06)",
+        },
+        text: {
+          heading: "rgba(255, 255, 255, 0.95)",
+          subheading: "rgba(255, 255, 255, 0.75)",
+          body: "rgba(255, 255, 255, 0.5)",
+          description: "rgba(255, 234, 204, 0.3)",
+        },
       },
       fontSize: {
         xxs: "0.6rem",
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        header:
+          "0px 8px 10px 0px rgba(0, 0, 0, 0.08), 0px 3px 16px 0px rgba(0, 0, 0, 0.08), 0px 4px 5px 0px rgba(0, 0, 0, 0.08)",
       },
     },
   },
