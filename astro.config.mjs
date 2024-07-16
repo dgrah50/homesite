c41cd48faa89cea8ff008ffe84e5c13bb92b156b
Dayan Graham <dayangraham@dayans-mbp.home>
2024-07-16 20:55:22 +0100

    (chore): Set dev baseurl to /.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+const isDev = process.env.NODE_ENV === "development";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
   site: "https://dgrah50.github.io",
-  base: "/homesite",
+  base: isDev ? "/" : "/homesite",
 });
