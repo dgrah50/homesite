9d08adeda92ff1fb9f48d315651346ca8a3b0ade
Dayan Graham <d.graham50@hotmail.co.uk>
2026-04-12 11:52:00 +0500

    chore: update Astro and fix dependency advisories

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), sitemap()],
+  integrations: [react(), mdx(), sitemap()],
   site: "https://dayangrah.am",
 });
